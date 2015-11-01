225ea58dd999187bb609c6edb242fd91c1a41e62
Yuto Otaguro <yotagur@gmail.com>
2015-11-01 03:24:47 -0600

    Apparently version 5.22 is not being used.
    - Reverted version number in api-constants to reflect version 5.21.
    - Fizz and Jax's thumbnails should be repaired now.

--- RiotAPI/api-constants.js
+++ RiotAPI/api-constants.js
@@ -7,7 +7,7 @@ const apiKey = fs.readFileSync('./../RiotAPI/api-key.txt');
 
 module.exports = {
     apiKey: apiKey,
-    currentVersion: '5.22.1',
+    currentVersion: '5.21.1',
     champDataURL:   'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?',
     versionsURL:    'https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?',
     jsonFilePath:   '../public/resources/data/',
