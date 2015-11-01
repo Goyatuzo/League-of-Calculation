4442eb289c4294b0c1dab3fa4ada5edb4f8c959b
Yuto Otaguro <yotagur@gmail.com>
2015-11-01 06:21:26 -0600

    Fixed paths to the files.
    - I set up the configurations in IntelliJ incorrectly.

--- RiotAPI/api-constants.js
+++ RiotAPI/api-constants.js
@@ -3,7 +3,7 @@
  */
 var fs = require('fs');
 
-const apiKey = fs.readFileSync('./../RiotAPI/api-key.txt');
+const apiKey = fs.readFileSync('./RiotAPI/api-key.txt');
 const imagePath = 'resources/images/';
 const jsonPath = 'resources/data/';
 
@@ -12,7 +12,7 @@ module.exports = {
     currentVersion: '5.21.1',
     champDataURL:   'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?',
     versionsURL:    'https://global.api.pvp.net/api/lol/static-data/na/v1.2/versions?',
-    jsonFilePath:   '../public/' + jsonPath,
-    imageFilePath:  '../public/' + imagePath,
+    jsonFilePath:   './public/' + jsonPath,
+    imageFilePath:  './public/' + imagePath,
     imageFilePathJade: imagePath
 };
