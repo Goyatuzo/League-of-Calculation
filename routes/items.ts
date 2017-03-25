cad295bfb89bbcb3a0211b92db860db50bd3533b
Yuto Otaguro <yotagur@gmail.com>
2017-03-25 02:06:41 -0500

    Bootstrap item data onto page on load.

--- routes/items.ts
+++ routes/items.ts
@@ -1,11 +1,12 @@
-﻿/*
- * GET home page.
- */
-import express = require('express');
+﻿import * as express from 'express';
+import ItemStaticEndpoint from '../RiotAPI/endpoints/items';
+
 const router = express.Router();
 
 router.get('/', (req: express.Request, res: express.Response) => {
-    res.render('index', { title: 'Express' });
+    ItemStaticEndpoint.getAllItems("NA").then(itemData => {
+        res.render('items', { title: 'All Items', bootstrap: itemData });
+    });
 });
 
 export default router;
